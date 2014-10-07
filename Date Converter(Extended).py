76e7361939de38b1012b426937caa8090050b6cc
JH101 <41425@longroad.ac.uk>
2014-10-07 13:00:45 +0100

    Work Done 07/10/14

--- Date Converter(Extended).py
+++ Date Converter(Extended).py
@@ -4,7 +4,25 @@
 
 date= input("Please enter the date in the form; ##/##/##(I.E 02/02/98): ")
 
-day = date[0:2]
+day_number = date[0:2]
+
+if day_number == "01":
+    day = "1st"
+
+if int(day_number) % 10 == 1 and int(day_number) != 11:
+    day = "{0}st".format(day_number)
+
+elif day_number == "02":
+    day = "2nd"
+
+if int(day_number) % 10 == 2 and int(day_number) != 12:
+    day = "{0}nd".format(day_number)
+
+elif int(day_number)> 31:
+    print("That is an invalid date, try again")
+
+else:
+    day = "{0}th".format(day_number)
 
 month_number = date[3:5]
 
@@ -45,7 +63,7 @@ elif month_number == "12":
     month = "December"
 
 else:
-    print("That is an invaild number")
+    print("That is an invaild Month, please try again")
 
 year_number = int(date[6:])
 
@@ -55,4 +73,8 @@ if year_number < 30:
 else:
     year = year_number + 1900
 
-print("The date you entered is the {0}th {1} {2}".format(day, month, year))
+if year % 4 == 0:
+    print("The date you entered is the {0} {1} {2} Which is also a leap year".format(day, month, year))
+    
+else:
+    print("The date you entered is the {0} {1} {2}".format(day, month, year))
